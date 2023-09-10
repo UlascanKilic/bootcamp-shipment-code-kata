75245f783d03b8a199a2b3f9daa029c93a22fc91
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2023-09-10 18:03:48 +0300

    initial commit

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,22 @@
 package com.trendyol.shipment;
 
-import java.util.List;
+import utils.Constants;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Basket {
-
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+        Map<ShipmentSize, Long> sizeCounts = getProductsReverseOrderedBySize();
+
+        if (sizeCounts.getOrDefault(ShipmentSize.X_LARGE, 0L) >= Constants.THRESHOLD_VALUE) {
+            return ShipmentSize.X_LARGE;
+        }
+
+        return findShipmentSize(sizeCounts);
+
     }
 
     public List<Product> getProducts() {
@@ -16,5 +25,23 @@ public class Basket {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    private Map<ShipmentSize, Long> getProductsReverseOrderedBySize(){
+        return getProducts().stream()
+                .collect(Collectors.groupingBy(
+                        Product::getSize,
+                        () -> new TreeMap<>(Comparator.reverseOrder()), //For edge case like -> S S S M M M L
+                        Collectors.counting()
+                ));
+    }
+
+    private ShipmentSize findShipmentSize(Map<ShipmentSize, Long> sizeCounts) {
+        for (Map.Entry<ShipmentSize, Long> entry : sizeCounts.entrySet()) {
+            if (entry.getValue() >= Constants.THRESHOLD_VALUE) {
+                return entry.getKey().getLarger();
+            }
+        }
+        return Collections.max(sizeCounts.keySet());
     }
 }
---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Constants {
+
+    public static int THRESHOLD_VALUE = 3;
+}
